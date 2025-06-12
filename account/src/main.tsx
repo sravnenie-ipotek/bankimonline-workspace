8cc2953c137c25dcd48b5c912ebfa9ada6a8906a
Michael <34490316+MichaelMishaev@users.noreply.github.com>
2025-06-12 03:29:17 +0300

    \Fix i18n loading with Suspense to prevent translation keys from showing"

--- account/src/main.tsx
+++ account/src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { I18nextProvider } from 'react-i18next'
 import { Provider } from 'react-redux'
@@ -8,11 +8,26 @@ import App from './app/App.tsx'
 import './index.scss'
 import store from './store'
 
+// Loading component while i18n loads
+const Loading = () => (
+  <div style={{ 
+    display: 'flex', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    height: '100vh',
+    fontSize: '18px'
+  }}>
+    Loading...
+  </div>
+)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
       <Provider store={store}>
-        <App />
+        <Suspense fallback={<Loading />}>
+          <App />
+        </Suspense>
       </Provider>
     </I18nextProvider>
   </React.StrictMode>
