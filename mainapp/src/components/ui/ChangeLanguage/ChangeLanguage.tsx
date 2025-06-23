3537fa1cba5e27372a40e401062d8d22c6e313b2
Michael Mishayev <michaelmishayev@Mac.lan>
2025-06-23 10:01:20 +0300

    Fix SMS Verification Code RTL Display Issue

--- mainapp/src/components/ui/ChangeLanguage/ChangeLanguage.tsx
+++ mainapp/src/components/ui/ChangeLanguage/ChangeLanguage.tsx
@@ -58,20 +58,20 @@ const ChangeLanguage = () => {
   const data = [
     {
       value: 'en',
-      country: 'United States',
-      language: 'English',
+      country: t('country_us'),
+      language: t('language_english'),
       icon: <USFlagIcon />,
     },
     {
       value: 'he',
-      country: 'ישראל',
-      language: 'עִברִית',
+      country: t('country_israel'),
+      language: t('language_hebrew'),
       icon: <IsraelFlagIcon />,
     },
     {
       value: 'ru',
-      country: 'Россия',
-      language: 'Русский',
+      country: t('country_russia'),
+      language: t('language_russian'),
       icon: <RussiaFlagIcon />,
     },
   ]
