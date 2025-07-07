5263cf916d8a2dba851d5fc2f3a2de4901af6f21
Michael Mishayev <michaelmishayev@Michaels-MacBook-Pro.local>
2025-07-07 13:21:20 +0300

    fix: tender page bug fixes
    - Replace missing image paths with existing illustration assets
    - Insert PercentIcon and adjust clients banner layout
    - Add earnings header section with translations (EN/HE/RU)
    - Improve clients banner styling

--- mainapp/src/pages/TendersForBrokers/TendersForBrokers.tsx
+++ mainapp/src/pages/TendersForBrokers/TendersForBrokers.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next'
 import Container from '../../components/ui/Container/Container'
 import { PartnersSwiper } from '@src/components/ui/Swiper'
+import PercentIcon from '../../assets/icons/PercentIcon/PercentIcon'
 import styles from './TendersForBrokers.module.scss'
 
 const TendersForBrokers = () => {
@@ -26,7 +27,7 @@ const TendersForBrokers = () => {
               {t('tenders_hero_cta')}
             </button>
           </div>
-          <img className={styles.heroImage} src="/static/tenders/hero-illustration.svg" alt="office" />
+          <img className={styles.heroImage} src="/static/illustration@2x.png" alt="office" />
         </section>
 
         {/* Marketplace Promo */}
@@ -39,12 +40,18 @@ const TendersForBrokers = () => {
               <li>{t('tenders_market_b2')}</li>
             </ul>
           </div>
-          <img src="/static/tenders/market-photo.svg" className={styles.marketPhoto} />
+          <img src="/static/illustration1@2x.png" className={styles.marketPhoto} alt="market" />
         </section>
 
         {/* We bring clients banner */}
         <section className={styles.clientsBanner}>
+          <PercentIcon color="#FBE54D" width={28} height={28} />
           <p>{t('tenders_clients_text')}</p>
+        </section>
+
+        {/* Earnings section header */}
+        <section className={styles.earnHeader}>
+          <h2>{t('tenders_earn_title')}</h2>
         </section>
 
         {/* About */}
