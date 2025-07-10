3e723941b7aa832838f5d98a2f5ef59b2c4046c3
Michael Mishayev <michaelmishayev@Michaels-MacBook-Pro.local>
2025-07-10 18:13:51 +0300

    fix: Resolve Railway build error with classnames/bind import
    - Change import from 'classnames/bind' to 'classnames' in SharedHeader
    - Fixes Railway build issue with submodule import resolution
    - Functionality remains identical, just different import syntax

--- bankEmployeeShared/components/SharedHeader/SharedHeader.tsx
+++ bankEmployeeShared/components/SharedHeader/SharedHeader.tsx
@@ -11,7 +11,7 @@
  */
 
 import React from 'react';
-import classNames from 'classnames/bind';
+import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
